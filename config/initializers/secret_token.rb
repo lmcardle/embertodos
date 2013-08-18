e46d4f68261e59b9650fe9c354d9b70777045974
Liam McArdle <mcardle.liam@gmail.com>
2013-08-18 10:45:20 -0700

    initial project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberTodos::Application.config.secret_key_base = 'ec78b29ab7e3ac27d6e13ee41149bf874e6aecfee4921b69dadd7f356bdd34535881aa5d589eadc0f6e7e6f69d91fe5b6cdb83aada84547392d3dbb93b1a9163'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberTodos::Application.config.session_store :cookie_store, key: '_ember_todos_session'
